20d79fd918bcc5f288eb7e87bdde553af7d6c336
Kushagraved <kushagraved@gmail.com>
2023-01-18 13:10:32 +0530

    chore: add testing

--- bowlingGameUtils.test.js
+++ bowlingGameUtils.test.js
@@ -1,30 +1,31 @@
+const {calculateScore,bestScore}=require('./bowlingGameUtils')
 describe('Bowling Game Utilities',()=>{
     describe('Calculate score of particular game', () => {
         const testCases = [
           {
             numberList: [10,5,5,9,0],
-            expected: -1
+            expected: 48
           },
           {
             numberList: [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6],
-            expected: 4
+            expected: 90
           }
           ,
           {
             numberList: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,3],
-            expected: 4
+            expected: 5
           }
           ,
           {
             numberList: [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            expected: 4
+            expected: 16
             
           }
         ];
       
         testCases.forEach(test => {
           it('', () => {
-            const res = arrayUtils.computeLargestEven(test.numberList);
+            const res = calculateScore(test.numberList);
             expect(res).toEqual(test.expected);
           });
         });
